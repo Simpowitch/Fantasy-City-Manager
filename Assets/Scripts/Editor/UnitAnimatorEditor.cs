bcedad02e7226f31ac85f6876d889c96d44ca25a
Simon Voss <55984457+Simwpowitch@users.noreply.github.com>
2020-10-02 18:30:26 +0200

    Animations for all units

---
+++ Assets/Scripts/Editor/UnitAnimatorEditor.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(UnitAnimator))]
+public class UnitAnimatorEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        UnitAnimator inspectorInstance = (UnitAnimator)target;
+
+        if (GUILayout.Button("Show Down")) //DOWN
+        {
+            inspectorInstance.ChangeSpriteDirection(Direction2D.S);
+        }
+        if (GUILayout.Button("Show Right")) //RIGHT
+        {
+            inspectorInstance.ChangeSpriteDirection(Direction2D.E);
+        }
+        if (GUILayout.Button("Show Left")) //LEFT
+        {
+            inspectorInstance.ChangeSpriteDirection(Direction2D.W);
+        }
+        if (GUILayout.Button("Show Up")) //UP
+        {
+            inspectorInstance.ChangeSpriteDirection(Direction2D.N);
+        }
+    }
+}
--- Assets/Scripts/Units/UnitAnimator.cs
+++ Assets/Scripts/Units/UnitAnimator.cs
@@ -4,27 +4,39 @@ using UnityEngine;
 
 public class UnitAnimator : MonoBehaviour
 {
-    //[Header("Renderers and sprites")]
-    //[SerializeField] SpriteRenderer body = null;
-    //[SerializeField] SpriteRenderer head = null;
-    //[SerializeField] SpriteRenderer lHand = null;
-    //[SerializeField] SpriteRenderer lFoot = null;
-    //[SerializeField] SpriteRenderer rHand = null;
-    //[SerializeField] SpriteRenderer rFoot = null;
+    [Header("Renderers and sprites")]
+    [SerializeField] SpriteRenderer body = null;
+    [SerializeField] SpriteRenderer head = null;
+    [SerializeField] SpriteRenderer lHand = null;
+    [SerializeField] SpriteRenderer lFoot = null;
+    [SerializeField] SpriteRenderer rHand = null;
+    [SerializeField] SpriteRenderer rFoot = null;
 
-    //[SerializeField] Sprite[] bodies = null;
-    //[SerializeField] Sprite[] heads = null;
-    //[SerializeField] Sprite[] lHands = null;
-    //[SerializeField] Sprite[] lFeet = null;
-    //[SerializeField] Sprite[] rHands = null;
-    //[SerializeField] Sprite[] rFeets = null;
+    [SerializeField] Sprite[] bodies = new Sprite[3];
+    [SerializeField] Sprite[] heads = new Sprite[3];
+    [SerializeField] Sprite[] lHands = new Sprite[3];
+    [SerializeField] Sprite[] lFeet = new Sprite[3];
+    [SerializeField] Sprite[] rHands = new Sprite[3];
+    [SerializeField] Sprite[] rFeet = new Sprite[3];
+
+
+    [SerializeField] int[] headOrderInLayer = new int[4];
+    [SerializeField] int[] lHandOrderInLayer = new int[4];
+    [SerializeField] int[] rHandOrderInLayer = new int[4];
+    [SerializeField] int[] lFootOrderInLayer = new int[4];
+    [SerializeField] int[] rFootOrderInLayer = new int[4];
+
 
     [SerializeField] GameObject carryObjectHolder = null;
 
     [Header("Animations")]
     [SerializeField] Animator animator = null;
     [SerializeField] string idle = "idle";
-    [SerializeField] string walk = "walking";
+    [SerializeField] string walkUp = "walkUp";
+    [SerializeField] string walkRight = "walkRight";
+    [SerializeField] string walkDown = "walkDown";
+    [SerializeField] string walkLeft = "walkLeft";
+
     [SerializeField] string harvest = "harvest";
     [SerializeField] string chop = "chop";
     [SerializeField] string mine = "mine";
@@ -40,7 +52,7 @@ public class UnitAnimator : MonoBehaviour
 
     public void PlayIdleAnimation(Vector3 dir)
     {
-        ChangeSpriteDirections(dir);
+        ChangeSpriteDirection(Direction2D.S);
         if (lastTag == idle)
             return;
         lastTag = idle;
@@ -49,17 +61,18 @@ public class UnitAnimator : MonoBehaviour
 
     public void PlayWalkAnimation(Vector3 dir)
     {
-        ChangeSpriteDirections(dir);
-        if (lastTag == walk)
+        Direction2D direction = ChangeSpriteDirection(dir);
+        string newTag = GetWalkTag(direction);
+        if (lastTag == newTag)
             return;
-        lastTag = walk;
+        lastTag = newTag;
         animator.SetTrigger(lastTag);
     }
 
 
     public void PlayHarvestFoodAnimation(Vector3 dir)
     {
-        ChangeSpriteDirections(dir);
+        ChangeSpriteDirection(Direction2D.S);
         if (lastTag == harvest)
             return;
         lastTag = harvest;
@@ -68,7 +81,7 @@ public class UnitAnimator : MonoBehaviour
 
     public void PlayMiningAnimation(Vector3 dir)
     {
-        ChangeSpriteDirections(dir);
+        ChangeSpriteDirection(Direction2D.S);
         if (lastTag == mine)
             return;
         lastTag = mine;
@@ -77,7 +90,7 @@ public class UnitAnimator : MonoBehaviour
 
     public void PlayWoodChopAnimation(Vector3 dir)
     {
-        ChangeSpriteDirections(dir);
+        ChangeSpriteDirection(Direction2D.S);
         if (lastTag == chop)
             return;
         lastTag = chop;
@@ -86,7 +99,7 @@ public class UnitAnimator : MonoBehaviour
 
     public void PlayPlantSeedAnimation(Vector3 dir)
     {
-        ChangeSpriteDirections(dir);
+        ChangeSpriteDirection(Direction2D.S);
         if (lastTag == plant)
             return;
         lastTag = plant;
@@ -95,7 +108,7 @@ public class UnitAnimator : MonoBehaviour
 
     public void PlayBuildAnimation(Vector3 dir)
     {
-        ChangeSpriteDirections(dir);
+        ChangeSpriteDirection(Direction2D.S);
         if (lastTag == build)
             return;
         lastTag = build;
@@ -112,10 +125,99 @@ public class UnitAnimator : MonoBehaviour
         carryObjectHolder.SetActive(false);
     }
 
-    private void ChangeSpriteDirections(Vector3 dir)
+    //Converts a vector to a direction and then calls for sprite changes
+    private Direction2D ChangeSpriteDirection(Vector3 dir)
     {
+        Direction2D direction;
         if (dir == Vector3.zero)
-            return;
-        //Exchange sprites dependent on the movement
+            direction = Direction2D.S;
+        else
+        {
+            direction = Utility.GetDirection(this.transform.position, this.transform.position + dir);
+        }
+        ChangeSpriteDirection(direction);
+        return direction;
+    }
+    //Exchange sprites dependent on the direction of the action/movement
+    public void ChangeSpriteDirection(Direction2D direction)
+    {
+        int spriteIndex = 0;
+        int animatorDirectionIndex = 0;
+        bool flip = false;
+        switch (direction)
+        {
+            case Direction2D.N:
+                spriteIndex = 2;
+                animatorDirectionIndex = 0;
+                break;
+            case Direction2D.NE:
+            case Direction2D.E:
+            case Direction2D.SE:
+                spriteIndex = 1;
+                animatorDirectionIndex = 1;
+                break;
+            case Direction2D.S:
+                spriteIndex = 0;
+                animatorDirectionIndex = 2;
+                break;
+            case Direction2D.SW:
+            case Direction2D.W:
+            case Direction2D.NW:
+                spriteIndex = 1;
+                animatorDirectionIndex = 3;
+                flip = true;
+                break;
+        }
+
+
+        //Set orders in layer to correctly place object infront or behind other objects
+        head.sortingOrder = headOrderInLayer[animatorDirectionIndex];
+        lHand.sortingOrder = lHandOrderInLayer[animatorDirectionIndex];
+        rHand.sortingOrder = rHandOrderInLayer[animatorDirectionIndex];
+        lFoot.sortingOrder = lFootOrderInLayer[animatorDirectionIndex];
+        rFoot.sortingOrder = rFootOrderInLayer[animatorDirectionIndex];
+
+        //Flip sprite renderer dependent on left/right
+        head.flipX = flip;
+        body.flipX = flip;
+        lFoot.flipX = flip;
+        rFoot.flipX = flip;
+        lHand.flipX = flip;
+        rHand.flipX = flip;
+
+        //Change sprites
+        if (heads.Length > 0)
+            head.sprite = heads[spriteIndex];
+        if (bodies.Length > 0)
+            body.sprite = bodies[spriteIndex];
+        if (lFeet.Length > 0)
+            lFoot.sprite = lFeet[spriteIndex];
+        if (rFeet.Length > 0)
+            rFoot.sprite = rFeet[spriteIndex];
+        if (lHands.Length > 0)
+            lHand.sprite = lHands[spriteIndex];
+        if (rHands.Length > 0)
+            rHand.sprite = rHands[spriteIndex];
+    }
+
+    private string GetWalkTag(Direction2D direction)
+    {
+        switch (direction)
+        {
+            case Direction2D.N:
+                return walkUp;
+            case Direction2D.NE:
+            case Direction2D.E:
+            case Direction2D.SE:
+                return walkRight;
+            case Direction2D.S:
+                return walkDown;
+            case Direction2D.SW:
+            case Direction2D.W:
+            case Direction2D.NW:
+                return walkLeft;
+            default:
+                return "";
+        }
     }
 }
