0f4a0276b4eaf378aa451a4f0eecc704230cd319
Simon Voss <55984457+Simwpowitch@users.noreply.github.com>
2020-11-12 16:21:53 +0100

    Search system for nodes and links in a dialogue

--- Assets/SimonPackages/Dialogue/Runtime/DialogueContainer.cs
+++ Assets/SimonPackages/Dialogue/Runtime/DialogueContainer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 [Serializable]
@@ -8,4 +9,28 @@ public class DialogueContainer : ScriptableObject
 {
     public List<NodeLinkData> nodeLinks = new List<NodeLinkData>();
     public List<DialogueNodeData> dialogueNodeDatas = new List<DialogueNodeData>();
+
+    public DialogueNodeData GetNodeByLinkTargetID(string targetNodeGUID)
+    {
+        foreach (var node in dialogueNodeDatas)
+        {
+            if (node.GUID == targetNodeGUID) return node;
+        }
+        Debug.LogError($"ID: {targetNodeGUID}, not found");
+        return null;
+    }
+
+    public List<DialogueNodeData> GetNextNodesFromNodeID(string nodeGUID)
+    {
+        List<DialogueNodeData> dialogueNodes = new List<DialogueNodeData>();
+        GetLinksByNodeID(nodeGUID).ForEach(link => dialogueNodes.Add(GetNodeByLinkTargetID(link.targetNodeGUID)));
+        return dialogueNodes;
+    }
+
+    public List<NodeLinkData> GetLinksByNodeID(string nodeGUID)
+    {
+        List<NodeLinkData> relatedNodes = new List<NodeLinkData>();
+        nodeLinks.Where(x => x.baseNodeGUID == nodeGUID).ToList().ForEach(foundLink => relatedNodes.Add(foundLink));
+        return relatedNodes;
+    }
 }
